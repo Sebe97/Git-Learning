720973bfc2939c4088eb9607fdee384956075514
Sebe97 <sebe97@yahoo.com>
2021-05-20 19:43:48 +0300

    Commit la la 2 lea fiiser din interfata

--- Al 2 lea fisier.js
+++ Al 2 lea fisier.js
@@ -1,2 +1,7 @@
 Acesta este l 2 lea fisier creat de mine
 Am sa incerc sa il pushuiesc din lina de comanda
+Se pare ca e destul de simplu sa fac un commit si din linia de comanda
+Pasii pe care trebuie sa ii urmez pentur a face commit si push din linia de comanda sunt:
+1. git add .
+2. git commit -m "mesajul comitului"
+3. git push
