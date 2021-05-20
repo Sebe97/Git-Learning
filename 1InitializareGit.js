2879a6715c5d915c99b3fa1e4f8b6a5b5c20c296
Sebe97 <sebe97@yahoo.com>
2021-05-20 19:55:10 +0300

    lucrurile de baza ale gitului

---
+++ 1InitializareGit.js
@@ -0,0 +1,4 @@
+Pentru a initializa git procedam in felul urmator:
+1. Ne logam cu contul nostru de git(git config--g user.email sebe97@yahoo.com git config--g user.name Sebe97)
+Pentru a verifica credentialele curente folosim : git config user.email si git config user.name
+2. git init (aceasta comanda va adauga proiectului nostru git si vom putea sa folosim functionalitatea git-ului)
--- fisier1.js
+++
@@ -1,3 +0,0 @@
-primul rand
-a 2 a modificare
-a 3 a modificare, iar de data asta am sa folosesc linia de comanda pentru a commite modificarile
